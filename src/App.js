dbb0ba7ca2262af6025e9ee719193a8405375374
HamzaBhf00 <alumno.1248865@ies-azarquiel.es>
2023-06-05 20:58:02 +0200

    Mapear el resultado, estilo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+import React, { useState, useEffect } from "react";
 import Cards from "./components/Cards/Cards";
 
 
-
 function App() {
+
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+  let api = `https://rickandmortyapi.com/api/character/?page=1`
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return <div className="App">
-
-    <h1 className="text-center ubuntu my-5">Rick & Morty <span className="text-primary">React</span></h1>
-    <div className="container">
-      <div className="row justify-content-center text-center">
-  <Cards/>
-        
-
+    <div className="App">
+      <h1 className="text-center mb-3">Characters</h1>
+      <div className="container">
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              <Cards results={results}/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
